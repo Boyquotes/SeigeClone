ad69612bb10880e9ba81b666c99cdfe3cfc57f0c
IcePanorama <87292274+IcePanorama@users.noreply.github.com>
2023-05-06 13:54:40 -0400

    Fixed collisions continuing after game over

--- Scripts/ball.gd
+++ Scripts/ball.gd
@@ -1,8 +1,15 @@
 extends CharacterBody3D
 class_name ball
 
+@export var _collision_shape : CollisionShape3D
+
 var _is_falling : bool = false
 var _gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+
+func _process(_delta):
+	if GameState.is_game_over():
+		_collision_shape.set_disabled(true)
 
 
 func _physics_process(delta):
